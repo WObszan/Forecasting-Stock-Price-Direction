61fb8f53e12fb44dd13898ef56c916a6f87a16b5
WObszan <obszanskiwojciech@gmail.com>
2026-01-25 15:28:55 +0100

    feat: add optuna to optimized

--- main_model/model_functions.py
+++ main_model/model_functions.py
@@ -49,19 +49,50 @@ def get_target(input_df, ticker):
     return df
 
 
+def get_tbm_target(df, ticker, horizon=5, pt_sl=[1, 1.5]):
+    df = df.copy()
+    close = df[f'Close_{ticker}']
+
+    log_ret = np.log(close / close.shift(1))
+    volatility = log_ret.rolling(window=20).std()
+
+    targets = pd.Series(index=df.index, dtype=float)
+
+    for i in range(len(df) - horizon):
+        price_start = close.iloc[i]
+        current_vol = volatility.iloc[i]  ### dynamic barrier for each day
+
+        upper_barrier = price_start * (1 + current_vol * pt_sl[0])
+        lower_barrier = price_start * (1 - current_vol * pt_sl[1])
+
+        future_prices = close.iloc[i + 1: i + 1 + horizon]
+
+        targets.iloc[i] = 0
+
+        for price_future in future_prices:
+            if price_future >= upper_barrier:
+                targets.iloc[i] = 1  # profit taking hit
+                break
+            elif price_future <= lower_barrier:
+                targets.iloc[i] = -1  # stop loss hit
+                break
+    df['Target'] = targets
+    return df.dropna(subset=['Target'])
+
+
 ###
-def best_features(data_dict, tickers, statistics):
+def best_features(data_dict, tickers):
     warnings.filterwarnings('ignore')
     feature_dict = {}
 
     for i, share in enumerate(tickers):
         feature_dict[share] = {}
-        df = get_target(data_dict[share], share)
+        df = get_tbm_target(data_dict[share], share)
         features = [col for col in df.columns if col not in ['Target', 'index', 'DATE']]
 
         to_remove = [f'Volume_{share}_lag1', f'Volume_{share}_lag2', f'Volume_{share}_lag3', f'Volume_{share}_lag5',
-                         f'RSI_14_lag1', 'RSI_14_lag2', 'RSI_14_lag3', 'RSI_14_lag5', 'log_return_lag1', 'log_return_lag2',
-                         'log_return_lag3', 'log_return_lag5']
+                     f'RSI_14_lag1', 'RSI_14_lag2', 'RSI_14_lag3', 'RSI_14_lag5', 'log_return_lag1', 'log_return_lag2',
+                     'log_return_lag3', 'log_return_lag5']
 
         '''if share == 'AAPL':
             additional_to_remove = ['rolling_max_20', 'rolling_max_20', 'dist_to_max_20', 'dist_to_min_20', 'rolling_max_60', 'rolling_max_60', 'dist_to_max_60', 'dist_to_min_60']
@@ -83,6 +114,7 @@ def best_features(data_dict, tickers, statistics):
             n_jobs=-1,
             random_state=42,
             eval_metric='logloss',
+
             use_label_encoder=False
         )
 
@@ -95,39 +127,37 @@ def best_features(data_dict, tickers, statistics):
 
         model_judge = model_judge_rf
 
-        for stat in statistics:
-            cv_split = TimeSeriesSplit(n_splits=5)
+        cv_split = TimeSeriesSplit(n_splits=5)
+        min_feats = 15 if share == 'AAPL' else 10
+        rfecv = RFECV(
+            estimator=model_judge,
+            min_features_to_select=min_feats,
+            step=1,
+            cv=cv_split,
+            scoring='f1_weighted',
+            n_jobs=-1, )
 
-            min_feats = 10 if share == 'AAPL' else 7
-            rfecv = RFECV(
+        rfecv.fit(X, y)
+
+        print(f"Optimal features numer by RFECV : {rfecv.n_features_}")
+        selected_features = [f for f, s in zip(features, rfecv.support_) if s]
+
+        if share == 'AAPL' or share == 'MSFT':
+            X_refined = X[selected_features]
+            desired_features = 10
+            rfe_final = RFE(
                 estimator=model_judge,
-                min_features_to_select=min_feats,
-                step=1,
-                cv=cv_split,
-                scoring=stat,
-                n_jobs=-1)
+                n_features_to_select=desired_features,
+                step=1
+            )
 
-            rfecv.fit(X, y)
+            rfe_final.fit(X_refined, y)
+            final_aapl_features = [f for f, s in zip(selected_features, rfe_final.support_) if s]
 
-            print(f"Optimal features numer by RFECV : {rfecv.n_features_}")
-            selected_features = [f for f, s in zip(features, rfecv.support_) if s]
-
-            if share == 'AAPL' or share == 'MSFT':
-                X_refined = X[selected_features]
-                desired_features = 10
-                rfe_final = RFE(
-                    estimator=model_judge,
-                    n_features_to_select=desired_features,
-                    step=1
-                )
-
-                rfe_final.fit(X_refined, y)
-                final_aapl_features = [f for f, s in zip(selected_features, rfe_final.support_) if s]
-
-                selected_features = final_aapl_features
-            print(f"Winner features for {stat} ({len(selected_features)}):")
+            selected_features = final_aapl_features
+            print(f"Winner features: ({len(selected_features)}):")
             print(selected_features)
-            feature_dict[share][stat] = selected_features
+            feature_dict[share] = selected_features
 
     with open("../models_results/feature_dict.json", "w") as f:
         json.dump(feature_dict, f, indent=4)
